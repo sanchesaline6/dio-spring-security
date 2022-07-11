f243fbfb17b7a9e04b3ddd179f7a14347199ce81
aeoh <aline.sanches@gft.com>
2022-07-11 20:04:34 -0300

    Implements WelcomeController class for authorization page tests

---
+++ src/main/java/dio/diospringsecurity/WelcomeController.java
@@ -0,0 +1,26 @@
+package dio.diospringsecurity;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Welcome to My Spring Boot Web API";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('managers', 'users')")
+    public String users(){
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasRole('managers')")
+    public String managers(){
+        return "Authorized manager";
+    }
+}
