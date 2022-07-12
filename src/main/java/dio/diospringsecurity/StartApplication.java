09c21b227c0223430fa7048fde0fa2a33b07e760
aeoh <aline.sanches@gft.com>
2022-07-11 22:15:44 -0300

    Creates s StartApplication to set users and their respective roles

---
+++ src/main/java/dio/diospringsecurity/StartApplication.java
@@ -0,0 +1,36 @@
+package dio.diospringsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import javax.transaction.Transactional;
+
+public class StartApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if(user == null){
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("master123");
+            user.getRoles().add("managers");
+            repository.save(user);
+        }
+
+        user = repository.findByUsername("user");
+        if(user == null){
+            user = new User();
+            user.setName("USER");
+            user.setUsername("user");
+            user.setPassword("user123");
+            user.getRoles().add("users");
+            repository.save(user);
+        }
+    }
+}
--- src/main/java/dio/diospringsecurity/WebSecurityConfig.java
+++ src/main/java/dio/diospringsecurity/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package dio.diospringsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,12 +8,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private SecurityDatabaseService securityService;
+
+    @Autowired
+    public void globalUserDetailsService(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(securityService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+    /*
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -23,12 +33,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin")
                 .password("{noop}master123")
                 .roles("managers");
-    }
+    }*/
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         super.configure(web);
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,6 +48,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll()
                 .antMatchers("/managers").hasAnyRole("managers")
                 .antMatchers("/users").hasAnyRole("users", "managers")
-                .anyRequest().authenticated().and().formLogin();
+                .anyRequest().authenticated().and().httpBasic();
     }
 }
