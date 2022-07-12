860e62ed6aebb7188a8b8fcf77e185cd96d03845
aeoh <aline.sanches@gft.com>
2022-07-11 22:04:28 -0300

    Updates authentication with database

---
+++ src/main/java/dio/diospringsecurity/SecurityDatabaseService.java
@@ -0,0 +1,32 @@
+package dio.diospringsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SecurityDatabaseService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity == null){
+            throw new UsernameNotFoundException(username);
+        }
+
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        userEntity.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+        UserDetails user = new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), authorities) {
+        };
+
+        return user;
+    }
+}
